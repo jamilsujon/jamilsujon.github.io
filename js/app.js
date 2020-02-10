8ee999cff06a30c36fd77cff1b84f8ecee1b04a8
jamilsujon <ghosthimu@gmail.com>
2020-02-10 23:12:51 +0600

    water quality check v 1.0.2

--- js/app.js
+++ js/app.js
@@ -3,17 +3,37 @@ const tempCUi = document.getElementById('tempinC'),
     timeUi = document.getElementById('time'),
     turbidUi = document.getElementById('turbidity'),
     phUi = document.getElementById('phVal'),
-    waterAlertUi = document.getElementById('water-alert')
+    waterAlertUi = document.getElementById('water-alert'),
+    drinkingResult = document.getElementById('drinkingResult'),
+    aquaticResult = document.getElementById('aquaticResult'),
+    agricultureResult = document.getElementById('agricultureResult'),
     //processing returned object value. Keys name are random, so picking last key and return the value.
     processVal = (obj) => obj[Object.keys(obj)[Object.keys(obj).length - 1]];
 
-const setDangerUi = (val,upper,lower,elem) =>{
-    if(val<lower || val >upper){
-        elem.parentElement.className = 'properties bg-danger text-white'
+
+const checkPropertyLimit =(lower,upper,val) =>{
+    if(val<lower || val>upper){
         return false;
     }
-    elem.parentElement.className = 'properties'
     return true;
+}
+
+const purposeSuitablity =(purpose,idealpHRange,idealTRange,pH,T)=>{
+
+    const {lowerpH,upperpH} = idealpHRange;
+    const {lowerT,upperT} = idealTRange;
+    let txt;
+    let html;
+    if(checkPropertyLimit(lowerpH,upperpH,pH) && checkPropertyLimit(lowerT,upperT,T) ){
+        txt = `Suitable for ${purpose}`;
+        html = `<span class="text-success"> <i class="fas fa-check-circle"></i> ${txt}</span>`
+        return html;
+    }
+
+    txt = `Not Suitable for ${purpose}`;
+    html = `<span class="text-danger"> <i class="fas fa-times-circle"></i> ${txt}</span>`
+    return html
+
 }
 
 
@@ -30,11 +50,11 @@ const manipulateUi = ({ TempinC, TempinF, Time, Turbidity, pH }) => {
     timeUi.textContent = new Date(time).toLocaleTimeString();   
     turbidUi.textContent = turbidity;   
     phUi.textContent = phVal;  
-    //upper,lower,elem
-    const phRes = setDangerUi(phVal,8,6.5,phUi) 
-    const turbRes = setDangerUi(turbidity,5,0,turbidUi,phRes) 
-    waterAlertUi.innerHTML = (phRes && turbRes) ? '<span class="text-success">Water is safe to drink</span>': '<span class="text-danger">Water is not safe to drink</span>'
- 
+
+    drinkingResult.innerHTML = purposeSuitablity('drinking',{lowerpH:6.5,upperpH:8},{lowerT:0,upperT:5},phVal,turbidity);
+    aquaticResult.innerHTML = purposeSuitablity('aquatic organisms',{lowerpH:6,upperpH:9},{lowerT:0,upperT:25},phVal,turbidity);
+    agricultureResult.innerHTML = purposeSuitablity('Agriculture purpose',{lowerpH:6.5,upperpH:8.5},{lowerT:0,upperT:2},phVal,turbidity);
+    
 }
 
 
@@ -44,6 +64,4 @@ const ref = firebase.database().ref();
 ref.on('value', snapshot => {
     const val = snapshot.val();
     manipulateUi(val);
-    console.log(val)
-
 });
---
+++ rough.js
@@ -0,0 +1,14 @@
+// const setDangerUi = (val,upper,lower,elem) =>{
+//     if(val<lower || val >upper){
+//         elem.parentElement.className = 'properties bg-danger text-white'
+//         return false;
+//     }
+//     elem.parentElement.className = 'properties'
+//     return true;
+// }
+
+
+//upper,lower,elem
+    // const phRes = setDangerUi(phVal,8,6.5,phUi) 
+    // const turbRes = setDangerUi(turbidity,5,0,turbidUi,phRes) 
+    // waterAlertUi.innerHTML = (phRes && turbRes) ? '<span class="text-success">Water is safe to drink</span>': '<span class="text-danger">Water is not safe to drink</span>'
